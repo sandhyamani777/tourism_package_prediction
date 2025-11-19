2fbd89604951989c2d95cb38347c518fdd985f2b
sandhyamani777 <chandusandy2018@gmail.com>
2025-11-19 15:20:27 +0000

    first commit

--- tourism_project/deployment/app.py
+++ tourism_project/deployment/app.py
@@ -22,19 +22,19 @@ age= st.number_input("Age", min_value=18, max_value=100, value=30, step=1)
 typeofcontact = st.selectbox("Type of Contact", ["Company Invited", "Self Inquiry"])
 citytier = st.selectbox("City Tier", [1, 2, 3])
 durationofpitch = st.number_input("Duration of Pitch", min_value=1, max_value=100, value=10, step=1)
-occupation= st.selectbox("Occupation", ["Salaried", "Freelancer"])
+occupation= st.selectbox("Occupation", ["Salaried", "Free Lancer","Small Business","Large Business"])
 gender = st.selectbox("Gender", ["Male", "Female"])
 numberofpersonvisiting = st.number_input("Number of People Visiting", min_value=1, max_value=10, value=2, step=1)
 numberoffollowups = st.number_input("Number of Follow-ups", min_value=1, max_value=10, value=2, step=1)
 productpitched= st.selectbox("Product Pitched", ["Basic", "Deluxe","Standard","King","Super Deluxe"])
 preferredpropertystar= st.number_input("Preferred Property Star", min_value=1, max_value=5, value=3, step=1)
-maritalstatus = st.selectbox("Marital Status", ["Single", "Married", "Divorced"])
+maritalstatus = st.selectbox("Marital Status", ["Single", "Married", "Divorced","Unmarried"])
 numberoftrips = st.number_input("Number of Trips", min_value=1, max_value=10, value=2, step=1)
 passport = st.selectbox("Passport", [0, 1])
 pitchsatisfactionscore = st.number_input("Pitch Satisfaction Score", min_value=1, max_value=5, value=3, step=1)
 owncar = st.selectbox("Own Car", [0, 1])
 numberofchildrenvisiting = st.number_input("Number of Children Visiting", min_value=0, max_value=10, value=0, step=1)
-designation = st.selectbox("Designation", ["Executive", "Managerial", "Professional", "Other"])
+designation = st.selectbox("Designation", ["Executive", "Manager", "VP", "AVP","Senior Manager"])
 monthlyincome = st.number_input("Monthly Income", min_value=1000, max_value=100000, value=5000, step=100)
 
 # Assemble input into DataFrame
@@ -66,5 +66,5 @@ classification_threshold = 0.45
 if st.button("Predict"):
     prediction_proba = model.predict_proba(input_data)[0, 1]
     prediction = (prediction_proba >= classification_threshold).astype(int)
-    result = "Package Taken" if prediction == 1 else "Package not Taken"
+    result = "Taken" if prediction == 1 else "Not Taken"
     st.write(f"Based on the information provided, the customer is likely to {result}.")
